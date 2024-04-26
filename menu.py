e724fcbd3638a06f90f1562f2d78ed5e5f4db18f
Cody Dalton <cjdalton15@yahoo.com>
2024-04-26 11:57:17 -0400

    updated

--- menu.py
+++ menu.py
@@ -39,33 +39,7 @@ maximum = 200
 
 def get_font(size):
     return pygame.font.Font("assets/font.ttf", size)
-
-""" def play():
-    while True:
-        PLAY_MOUSE_POS = pygame.mouse.get_pos()
-
-        SCREEN.fill("black")
-
-        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
-        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
-
-        PLAY_BACK = Button(image=None, pos=(320, 230), 
-                            text_input="BACK", font=get_font(25), base_color=( 100, 100, 150 ), hovering_color="Green")
-
-        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
-        PLAY_BACK.update(SCREEN)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
-                    main_menu()
-
-        pygame.display.update() """
-    
+   
 def options():
     while True:
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
@@ -94,12 +68,7 @@ def options():
 
 
 def col_change_breathe(color: list, direction: list) -> None:
-    """
-    This function changes an RGB list in a way that we achieve nice breathing effect.
-    :param color: List of RGB values.
-    :param direction: List of color change direction values (-1, 0, or 1).
-    :return: None
-    """
+
     for i in range(3):
         color[i] += col_spd * direction[i]
         if color[i] >= maximum or color[i] <= minimum:
@@ -111,15 +80,7 @@ def col_change_breathe(color: list, direction: list) -> None:
 
 
 def draw_text(text: str, size: int, col: list, x: int, y: int) -> None:
-    """
-    A simple function for displaying text strings on the game screen.
-    :param text: The string to display.
-    :param size: Size of the text.
-    :param col: Color of the text.
-    :param x: X coordinate of the text.
-    :param y: Y coordinate of the text.
-    :return: None
-    """
+
     font_object = pygame.font.Font("assets/font.ttf", 45)
     text_surface = font_object.render(text, False, col)
     text_rect = text_surface.get_rect(center=(x, y))
@@ -132,9 +93,6 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        """ MENU_TEXT = get_font(33).render("ULTIMATE Tic-Tac-Toe", True, ( 221, 218, 191 ))
-        MENU_RECT = MENU_TEXT.get_rect(center=(360, 40)) """
-
         PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(360, 250), 
                             text_input="PLAY", font=get_font(30), base_color=( 221, 218, 191 ), hovering_color="Green")
         OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(360, 400), 
@@ -142,7 +100,6 @@ def main_menu():
         QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(360, 550), 
                             text_input="QUIT", font=get_font(30), base_color=( 221, 218, 191 ), hovering_color="Green")
 
-        """ SCREEN.blit(MENU_TEXT, MENU_RECT) """
 
         for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
